6afd7a8d3cbb64e9b9972520590a3e17a631243a
Marievi <maxez@intracom-telecom.com>
2017-10-17 16:31:24 +0300

    Changed license.

--- impl/src/main/java/sqmf/impl/MonitorLinksTask.java
+++ impl/src/main/java/sqmf/impl/MonitorLinksTask.java
@@ -1,9 +1,7 @@
 /*
  * Copyright © 2017 M.E Xezonaki in the context of her MSc Thesis, Department of Informatics and Telecommunications, UoA.  All rights reserved.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+
  */
 package sqmf.impl;
 
